022caa6e073ad1b33b2f9a8ad0cdefbded4da857
An <anluu099@gmail.com>
2022-02-26 02:24:50 +0700

    halo

---
+++ JS/js.js
@@ -0,0 +1,14 @@
+function bam(){
+    document.getElementById("love").style.display = "block";
+    document.getElementById("clc").style.display="none";
+    document.getElementById("clcc").style.display="none";
+    document.getElementById("box").style.display="none";
+}
+
+function re_chuot(obj){
+    obj.innerHTML = "💖Đồng ý💖"
+}
+
+function re_chuot1(obj){
+    obj.innerHTML = "😢Từ chối😢"
+}
